e7abfee1dbed0f38309dcc08321d05b8bec51e0e
masonkarsevar <masonkarsevar@gmail.com>
2020-03-17 14:55:47 -0700

    all tests pass for the stock price algorithm

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,7 +3,29 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+  # run a loop that will find the minimum profit price in the array 
+  # save the value and the index position in a variable 
+
+  # loop through the array again starting at the minimum price index and return
+  # the maximum price for the time period 
+
+  # if the minimum price is at the last index
+
+  # minus the different between the two prices.
+
+  profit = float('-inf')
+
+  for index_i in range(len(prices)):
+    for index_j in range(index_i+1, len(prices)):
+      profit_amount = prices[index_j] - prices[index_i] 
+      if profit < profit_amount:
+        profit = profit_amount
+
+  return profit
+
+
+print(find_max_profit([10, 7, 5, 8, 11, 9]))
+print(find_max_profit([100, 90, 80, 50, 20, 10]))
 
 
 if __name__ == '__main__':
